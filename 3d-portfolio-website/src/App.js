881dc3abbf6c5cba8c425f9e396d2eb6c5db123c
Vedant <guptavedant1995@yahoo.com>
2024-07-27 00:55:21 -0600

    started project section

--- 3d-portfolio-website/src/App.js
+++ 3d-portfolio-website/src/App.js
@@ -6,6 +6,7 @@ import Hero from "./components/Sections/Hero";
 import Skills from "./components/Sections/Skills";
 import Experience from "./components/Sections/Experience";
 import Education from "./components/Sections/Education";
+import Projects from "./components/Sections/Projects";
 
 const Body = styled.div`
   background-color: ${({ theme }) => theme.bg};
@@ -27,6 +28,7 @@ function App() {
           <Skills />
           <Experience />
           <Education />
+          <Projects />
         </Body>
       </BrowserRouter>
     </ThemeProvider>
